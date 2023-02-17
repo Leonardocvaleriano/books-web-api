a369bd990ca11879361e12118a354f04dbec6053
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2023-02-17 01:25:06 +0000

    feat: home activity implemented

--- app/src/main/java/com/codeplace/bookswebapi/ui/views/home/HomeActivity.kt
+++ app/src/main/java/com/codeplace/bookswebapi/ui/views/home/HomeActivity.kt
@@ -1,8 +1,6 @@
 package com.codeplace.bookswebapi.ui.views.home
 import android.os.Bundle
 import android.widget.ProgressBar
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
