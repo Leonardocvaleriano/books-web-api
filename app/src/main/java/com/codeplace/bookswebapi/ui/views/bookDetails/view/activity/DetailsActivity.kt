9134fe6ef03be1de2ed2df598888ea585285da90
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2023-02-23 20:17:42 +0000

    readjusting class files names part 2

--- app/src/main/java/com/codeplace/bookswebapi/ui/views/bookDetails/view/activity/DetailsActivity.kt
+++ app/src/main/java/com/codeplace/bookswebapi/ui/views/bookDetails/view/activity/DetailsActivity.kt
@@ -1,10 +1,13 @@
 package com.codeplace.bookswebapi.ui.views.bookDetails.view.activity
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.codeplace.bookswebapi.databinding.ActivityDetailsBinding
 import com.codeplace.bookswebapi.repository.BookDetailsRepository
+import com.codeplace.bookswebapi.ui.views.bookDetails.models.BookDetailslDto
 import com.codeplace.bookswebapi.ui.views.bookDetails.viewModel.BookDetailsViewModelFactory
 import com.codeplace.bookswebapi.ui.views.bookDetails.viewModel.BookDetailsViewModel
 
@@ -38,25 +41,27 @@ class DetailsActivity: AppCompatActivity() {
 
 
     //Call the repository to bring this id information
-    fun initValues(id:Int){
+    private fun initValues(id:Int){
         viewModel.id = id
         viewModel.getBook()
-
+        binding.progressBar.visibility = ProgressBar.VISIBLE
      }
 
-    fun initObservable(){
+   private fun initObservable(){
         viewModel.detailBookItens.observe(this){ bookDetailItems ->
-            with(binding){
-                txtTitle.text = bookDetailItems.title
-                txtAuthor.text = bookDetailItems.author
-                txtCurrency.text = bookDetailItems.currencyCode
-                txtPrice.text = bookDetailItems.price.toString()
-                txtIsbn.text = bookDetailItems.isbn
-                txtDescription.text = bookDetailItems.description
+            initBinding(bookDetailItems)
             }
-            }
+   }
 
-        }
-
-
-}
+    private fun initBinding(bookDetailItems:BookDetailslDto) {
+        with(binding) {
+            txtTitle.text = bookDetailItems.title
+            txtAuthor.text = bookDetailItems.author
+            txtCurrency.text = bookDetailItems.currencyCode
+            txtPrice.text = bookDetailItems.price.toString()
+            txtIsbn.text = bookDetailItems.isbn
+            txtDescription.text = bookDetailItems.description
+         }
+        binding.progressBar.visibility = ProgressBar.GONE
+    }
+ }
